ee6d7b83dbe1bbf1a560cb099367dedfea09dc6c
vishalkr101 <vishalkr00700@gmail.com>
2023-12-06 23:31:05 +0530

    gsap-trial removed completely

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,7 +1,7 @@
 import  './index.scss';
 import LogoS from '../../../assets/images/logo-s.png'
-import { useEffect, useRef } from 'react';
-import gsap from 'gsap-trial';
+import { useRef } from 'react';
+// import gsap from 'gsap-trial';
 // import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
 
 const Logo = () =>{
@@ -10,33 +10,33 @@ const Logo = () =>{
     const outlineLogoRef = useRef();
     const solidLogoRef = useRef();
 
-    useEffect(()=>{
-        // gsap.registerPlugin(DrawSVGPlugin)
+    // useEffect(()=>{
+    //     // gsap.registerPlugin(DrawSVGPlugin)
 
-        gsap
-        .timeline()
-        .to(bgRef.current, {
-            duration: 1,
-            opacity: 1
-        })
+    //     gsap
+    //     .timeline()
+    //     .to(bgRef.current, {
+    //         duration: 1,
+    //         opacity: 1
+    //     })
 
-        .from(outlineLogoRef.current, {
-            drawSVG: 0,
-            duration: 20,
-        })
+    //     .from(outlineLogoRef.current, {
+    //         drawSVG: 0,
+    //         duration: 20,
+    //     })
 
-        gsap.fromTo(
-            solidLogoRef.current,{
-                opacity: 0,
-            },
-            {
-                opacity: 1,
-                delay: 4,
-                duration: 4,
-            }
-        )
+    //     gsap.fromTo(
+    //         solidLogoRef.current,{
+    //             opacity: 0,
+    //         },
+    //         {
+    //             opacity: 1,
+    //             delay: 4,
+    //             duration: 4,
+    //         }
+    //     )
 
-    },[])
+    // },[])
     return (
         <div className='logo-container' ref={bgRef}>
             <img className='solid-logo' src={LogoS} alt='S' ref={solidLogoRef}/>
